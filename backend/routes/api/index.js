9e92f40ef8e1dc57b509005fb78cc5a6bbc11675
shtabesh02 <shtabesh02@gmail.com>
2023-10-17 19:13:40 -0400

    main

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -12,5 +12,24 @@ router.post('/test', function(req, res) {
   
   // ...
 
+
+
+  // backend/routes/api/index.js
+// ...
+
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+// ...
   
 module.exports = router;
